d315553e68798498e13f4eeb49575aefe400eac6
KevinBenaya <darkfaust063@gmail.com>
2022-03-09 14:07:01 +0700

    Extract+CallParameter(kevin_jobsheet4)

--- kevin_jobsheet4/lib/main.dart
+++ kevin_jobsheet4/lib/main.dart
@@ -38,8 +38,6 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  void konversiSuhu() {}
-
   TextEditingController retrive1 = TextEditingController();
 
   @override
@@ -52,97 +50,177 @@ class _MyHomePageState extends State<MyHomePage> {
         margin: EdgeInsets.all(8),
         child: Column(
           children: [
-            TextField(
-              controller: retrive1,
-              decoration: InputDecoration(
-                labelText: 'Celcius',
-                hintText: 'Masukkan Suhu Dalam Celcius',
-              ),
-              keyboardType: TextInputType.number,
-              inputFormatters: <TextInputFormatter>[
-                FilteringTextInputFormatter.digitsOnly
-              ],
-            ),
+            inputSuhu(retrive1: retrive1),
             SizedBox(height: 8),
-            DropdownButton(
-              isExpanded: true,
-              value: selectedDropdown,
-              items: listSatuanSuhu.map((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-              onChanged: (value) {
-                onDropDownChanged(value);
-              },
+            calculationTarget(
+              selectedDropdown: selectedDropdown,
+              listSatuanSuhu: listSatuanSuhu,
+              onDropdownChanged: onDropDownChanged,
             ),
             SizedBox(height: 10),
             Text(
               'Hasil',
               style: TextStyle(fontSize: 20),
             ),
-            Text(
-              '$calculationResult',
-              style: TextStyle(fontSize: 32),
-            ),
+            lastCalculation(calculationResult: calculationResult),
             SizedBox(height: 10),
-            Row(
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        if (retrive1.text.isNotEmpty) {
-                          calculationResult = double.parse(retrive1.text) * 2;
-                          switch (selectedDropdown) {
-                            case "Kelvin":
-                              calculationResult =
-                                  double.parse(retrive1.text) + 273;
-                              listHasil.add('Kelvin');
-                              break;
-                            case "Reamur":
-                              calculationResult =
-                                  double.parse(retrive1.text) * (4 / 5);
-                              listHasil.add('Reamur');
-                              break;
-                            case "Fahrenheit":
-                              calculationResult =
-                                  (double.parse(retrive1.text) * ((9 / 5)) +
-                                      32);
-                              listHasil.add('Fahrenheit');
-                              break;
-                          }
-                          listHasil.add("Konversi dari " +
-                              retrive1.text +
-                              " Celcius ke " +
-                              selectedDropdown +
-                              " Dengan Hasil : " +
-                              calculationResult.toString());
-                        }
-                      });
-                    },
-                    child: Text('Konversi Suhu'),
-                  ),
-                ),
-              ],
+            temperaturConvertion(
+              onPressed: konversiSuhu,
             ),
             SizedBox(height: 10),
             Text(
               'Riwayat Konversi',
               style: TextStyle(fontSize: 20),
             ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: listHasil.length,
-                itemBuilder: (context, index) {
-                  return Text(listHasil[index]);
-                },
-              ),
-            ),
+            calculationHistory(listHasil: listHasil),
           ],
         ),
       ),
+    );
+  }
+
+  void konversiSuhu() {
+    return setState(() {
+      if (retrive1.text.isNotEmpty) {
+        calculationResult = double.parse(retrive1.text) * 2;
+        switch (selectedDropdown) {
+          case "Kelvin":
+            calculationResult = double.parse(retrive1.text) + 273;
+            listHasil.add('Kelvin');
+            break;
+          case "Reamur":
+            calculationResult = double.parse(retrive1.text) * (4 / 5);
+            listHasil.add('Reamur');
+            break;
+          case "Fahrenheit":
+            calculationResult = (double.parse(retrive1.text) * ((9 / 5)) + 32);
+            listHasil.add('Fahrenheit');
+            break;
+        }
+        listHasil.add("Konversi dari " +
+            retrive1.text +
+            " Celcius ke " +
+            selectedDropdown +
+            " Dengan Hasil : " +
+            calculationResult.toString());
+      }
+    });
+  }
+}
+
+class temperaturConvertion extends StatelessWidget {
+  const temperaturConvertion({
+    Key? key,
+    required this.onPressed,
+  }) : super(key: key);
+
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: ElevatedButton(
+            onPressed: () {
+              onPressed();
+            },
+            child: Text('Konversi Suhu'),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class calculationTarget extends StatelessWidget {
+  const calculationTarget({
+    Key? key,
+    required this.selectedDropdown,
+    required this.listSatuanSuhu,
+    required this.onDropdownChanged,
+  }) : super(key: key);
+
+  final String selectedDropdown;
+  final List<String> listSatuanSuhu;
+  final Function onDropdownChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton(
+      isExpanded: true,
+      value: selectedDropdown,
+      items: listSatuanSuhu.map((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+      onChanged: (value) {
+        onDropdownChanged(value);
+      },
+    );
+  }
+}
+
+class calculationHistory extends StatelessWidget {
+  const calculationHistory({
+    Key? key,
+    required this.listHasil,
+  }) : super(key: key);
+
+  final List<String> listHasil;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: listHasil.length,
+        itemBuilder: (context, index) {
+          return Text(listHasil[index]);
+        },
+      ),
+    );
+  }
+}
+
+class lastCalculation extends StatelessWidget {
+  const lastCalculation({
+    Key? key,
+    required this.calculationResult,
+  }) : super(key: key);
+
+  final double calculationResult;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '$calculationResult',
+      style: TextStyle(fontSize: 32),
+    );
+  }
+}
+
+class inputSuhu extends StatelessWidget {
+  const inputSuhu({
+    Key? key,
+    required this.retrive1,
+  }) : super(key: key);
+
+  final TextEditingController retrive1;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: retrive1,
+      decoration: InputDecoration(
+        labelText: 'Celcius',
+        hintText: 'Masukkan Suhu Dalam Celcius',
+      ),
+      keyboardType: TextInputType.number,
+      inputFormatters: <TextInputFormatter>[
+        FilteringTextInputFormatter.digitsOnly
+      ],
     );
   }
 }
